1b2fec55573c8cc46be4af7e0b2f8cd9df7d0830
Mayank Sharma <Mayank.sharma0@walmart.com>
2021-04-20 23:00:54 +0530

    Touch

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
+const port = 9000;
 app.use(cors());
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.post('/test', (req, res) => {
+  res.send(req.body);
 });
 
-app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`),
+app.get('/test-get-body', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`),
 );
